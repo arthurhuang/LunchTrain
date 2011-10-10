2dabd7122cbf28bb84cbd9dbb9cdd9ede8e20dab
danicashei <danicashei@10.10.67.38>
2011-10-10 13:50:18 -0700

    change tabs to be dynamic

--- profile.php
+++ profile.php
@@ -1,7 +1,4 @@
-<?php 
-	ob_start();
-	session_start();
-?>
+<?php include "base.php";?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">  
 <head>  
@@ -31,12 +28,42 @@
 	 	</div>
 	 	<div id="right">
 	 		<div id="rightbody">
-	 			<header id="header">
-	 				<li><a href="#">Trains</a></li>
-	 				<li><a href="#">About Me</a></li>
-			 		<li><a href="#">Friends</a></li>
-	 			</header>
+				<div id="righttitle">
+	 				<header id="header">
+	 					<li><a href="profile.php?tab=1">Trains</a></li>
+	 					<li><a href="profile.php?tab=2">About Me</a></li>
+			 			<li><a href="profile.php?tab=3">Friends</a></li>
+	 				</header>
+	 			</div>
+	 			<div id="rightbottom">
+	 			<?php 
+					$tab = $_GET['tab'];
+					if ($tab == 1 || $tab == "") { ?>
+						<h2>Current trains:</h2>
+		
+					<?php
+						$result =mysql_query("SELECT * FROM Trains");
+						if (!$result) {
+							$message  = 'Invalid query: ' . mysql_error() . "\n";
+							$message .= 'Whole query: ' . $query;
+							die($message);
+						}
+						while ($row = mysql_fetch_assoc($result)) {
+							echo "<p> <b>Train name</b>: {$row['trainName']}, departure time: {$row['departureTime']}, meeting place: {$row['meetingPlace']}, transportation: {$row['transportType']},spaces available: {$row['spaceAvailable']} </p>";
+							echo "<p> <b>Train description</b>: {$row['trainDescription']} </p>";
+								    echo "<br>";
+						}
+					}
+					elseif ($tab == 2) { ?>
+		 				<p>About Me</p>	
+	 				<?php } 
+	 				elseif ($tab == 3) { ?>
+	 					<p>Friends</p>	
+	 				<?php } ?>
+	 				
 	 		</div>
+	 		</div>
+	 		
 	 	</div>
 	 </div>
 </body>
